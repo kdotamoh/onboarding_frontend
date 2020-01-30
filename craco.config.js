38e99d898d9b7fd4decfff07ad868970402878e1
kdotamoh <i.be.your.strychnine@gmail.com>
2020-01-30 21:36:31 +0000

    update craco config

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,17 @@
-const CracoAntDesignPlugin = require('craco-antd')
+const { isEqual } = require('lodash')
 
 module.exports = {
   babel: {
     plugins: ['babel-plugin-styled-components']
+  },
+  webpack: {
+    configure(webpackConfig) {
+      const updatedRules = webpackConfig.module.rules.filter(
+        rule => !isEqual(rule, { parser: { requireEnsure: false } })
+      )
+      webpackConfig.module.rules = updatedRules
+
+      return webpackConfig
+    }
   }
 }
