dfea9f1dbfc8f4c4d0b06f6e2b83a06dd489dc44
kdotamoh <i.be.your.strychnine@gmail.com>
2020-01-31 12:38:56 +0000

    add video

--- src/pages/onboarding/about-mtn/Tasks.js
+++ src/pages/onboarding/about-mtn/Tasks.js
@@ -6,6 +6,7 @@ import { Button, Img } from 'components/styled'
 import PageStyle from '../PageStyle'
 
 import bgImg from 'images/onboarding/bg_bottomright.svg'
+import codeofethics from 'assets/codeofethics.mp4'
 
 const BgImg = styled(Img)`
   position: absolute;
@@ -21,18 +22,17 @@ export default class Tasks extends Component {
       <>
         <PageStyle>
           <h3>Tasks</h3>
-          <h4>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-            sint porro nulla officiis, hic aut quam eius, earum eos illum quas,
-            natus ab repellendus assumenda quidem consequatur autem provident
-            iste.
-          </h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            qui debitis pariatur fuga blanditiis, tenetur doloribus fugiat iure
-            cupiditate enim esse ea sit? Impedit quaerat totam blanditiis illum,
-            quae cum.
-          </p>
+          <h4>Text here</h4>
+          <p>Text here</p>
+          <video
+            controls
+            css={`
+              width: 65rem;
+            `}
+          >
+            <source src={codeofethics} type="video/mp4" />
+            Sorry, your browser doesn't support embedded videos.
+          </video>
           <Link to="../../functional-groups/overview">
             <Button mt="15rem" color="blue">
               Functional Groups >
