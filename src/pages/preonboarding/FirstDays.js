9a86dd9084e49133fd185b60156165ab93174674
kdotamoh <i.be.your.strychnine@gmail.com>
2020-03-10 16:02:50 +0000

    fetch first days

--- src/pages/preonboarding/FirstDays.js
+++ src/pages/preonboarding/FirstDays.js
@@ -46,6 +46,12 @@ const ModalHeading = styled.h3`
   margin-bottom: 2rem;
 `
 
+const FirstDaysSection = styled(Section)`
+  h2: {
+    margin-top: 300px;
+  }
+`
+
 class FirstDays extends Component {
   state = {
     visible: false,
@@ -78,94 +84,30 @@ class FirstDays extends Component {
   render() {
     const { visible } = this.state
 
+    const { title } = this.props.pageContent ? this.props.pageContent : {}
+    const { hero_text } = this.props.pageContent ? this.props.pageContent : {}
+    const { content } = this.props.pageContent ? this.props.pageContent : {}
+
     return (
       <div>
         <SmallNav />
         <StepNav />
         <Container>
-          <H3 py="3rem">Your First Three Days</H3>
+          <H3 py="3rem" dangerouslySetInnerHTML={{ __html: title }}></H3>
           <Hero>
             <div className="row">
-              <p className="column">
-                We’re super excited and preparing towards your first day. You
-                will be taken through a three-day formal onboarding programme,
-                after which your onboarding will continue within your
-                department.
-              </p>
+              <p
+                className="column"
+                dangerouslySetInnerHTML={{ __html: hero_text }}
+              ></p>
               <img className="column" src={heroImg} alt="" />
             </div>
           </Hero>
 
           <Wrapper width="60%">
-            <Section>
-              <h2>Your First Day</h2>
-              <strong>Venue:</strong> MTN House, #6 Independence Avenue, West
-              Ridge, Accra <br />
-              <strong>Digital Address:</strong> GA-052-4025 <br />
-              <strong>Time: </strong> Do plan to arrive at MTN House by 8:00am
-              on your first day. <br />
-              <strong>Parking:</strong> In order to assist you with parking on
-              your first day, please present your ID to the security personnel
-              at the main entrance.
-            </Section>
-
-            <Section>
-              <h2>At the Reception</h2>
-              <p>
-                Present your ID to the receptionist. Your HR Business Partner
-                will meet you and usher you to the Induction Centre. To ensure
-                your safety and security, please cooperate with the security
-                personnel, as you will be taken through some security protocols.
-              </p>
-            </Section>
-
-            <Section>
-              <h2>At the Induction Centre</h2>
-              <p>
-                We’d like you to find your way around the office as quickly as
-                possible, so please expect an office facility tour on your first
-                day.
-              </p>
-              <p>
-                To activate your access to the office, your biometric details
-                (fingerprints), will be taken within your first two days.
-              </p>
-              <p>
-                It is important for you to understand what is required from you
-                as far as employee conduct is concerned. Kindly expect to be
-                taken through the following, among others, on your first day:
-              </p>
-              <ul>
-                <li>Employee Conduct Pledge</li>
-                <li>Confidentiality and Non-Disclosure Agreement</li>
-                <li>Employee Conflict of Interest Process</li>
-                <li>Compliance Policy</li>
-                <li>Anti Bribery &amp; Corruption Policy</li>
-                <li>Whistle Blowing Policy</li>
-                <li>Gift, Hospitality &amp; Entertainment Policy</li>
-                <li>Privacy Policy</li>
-                <li>Insider Trading Policy</li>
-              </ul>
-              <p>
-                We’d like you to acquaint yourself with our systems as quickly
-                as possible, so please expect a hands-on system induction
-                session on your second day. Customer Experience is at the heart
-                of our business. We’d love for you to meet and interact with our
-                customers as part of our Frontline Immersion programme for
-                employees. Please expect to pay a visit to any of our service
-                centres within your first week. Your feedback will go a long way
-                to help us improve on our services.
-              </p>
-              <p>
-                Your HR Business Partner will hand over the following to you
-                within your first three days at work:
-              </p>
-              <ul>
-                <li>Employee ID Card</li>
-                <li>Employee SIM Card</li>
-                <li>Medical Insurance Card</li>
-              </ul>
-            </Section>
+            <FirstDaysSection
+              dangerouslySetInnerHTML={{ __html: content }}
+            ></FirstDaysSection>
           </Wrapper>
 
           <Button color="blue" onClick={() => this.setState({ visible: true })}>
@@ -219,9 +161,11 @@ class FirstDays extends Component {
   }
 }
 FirstDays.propTypes = {
-  token: PropTypes.string
+  token: PropTypes.string,
+  pageContent: PropTypes.object
 }
 
 export default connect(state => ({
-  token: state.token
+  token: state.token,
+  pageContent: state.pages.preonboardingPages.firstDays
 }))(FirstDays)
