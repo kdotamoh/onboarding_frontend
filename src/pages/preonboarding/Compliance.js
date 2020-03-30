202c22b3a709e26099d77badf84ff0db75029a4c
kdotamoh <i.be.your.strychnine@gmail.com>
2020-03-30 10:35:38 +0000

    sort by order

--- src/pages/preonboarding/Compliance.js
+++ src/pages/preonboarding/Compliance.js
@@ -66,136 +66,6 @@ const Section = styled.section`
   }
 `
 
-// const sections = [
-//   {
-//     heading: 'General Conduct',
-//     img: conduct,
-//     text: `<p>
-//       What is required of us? How do we achieve this? Why is this important? At all times we are professional,
-//       respect the traditions and culture of all people, and refrain from behavior that’s offensive, intimidating, malicious or discriminatory.
-//       </p>
-//       `
-//   },
-//   {
-//     heading: 'Property and Information',
-//     img: property,
-//     text: `<p>We endeavor to put measures in place to ensure that our property and equipment are safe and secure at all times.
-//     Company resources should only be used for lawful business purposes and Employees are required to safeguard MTN against wasteful practices.</p>
-//     `
-//   },
-//   {
-//     heading: 'Intellectual Property',
-//     img: ip,
-//     text: `<p>You are the custodian of our brand so it’s up to you to ensure
-//     that we remain consistent – from Afghanistan to Zambia, and everywhere between.</p>`
-//   },
-//   {
-//     heading: 'Whistleblowing',
-//     img: whistleblowing,
-//     text: `<p>“MTN sees whistleblowing differently, regarding it as a positive practice that assists the organization to detect incidents of fraud,
-//        misconduct, and illegal activity early.”</p>  <p><strong>Q&A</strong></br><strong>Q</strong>: How do I report suspicious or illegal activity?
-//        </br> <strong>A</strong>: You may make use of our reporting channels such as: </br>  The MTN Fraud Hotline: 1385 </br> Email: anonymous@tip-offs.com
-//        </br> You may also refer to your local Risk and Compliance department for further information.</p>  Read the Whistleblowing Policy here <a href="https://bit.ly/2Y4KPwh">https://bit.ly/2Y4KPwh</a>`
-//   },
-//   {
-//     heading: 'Anti-Bribery & Corruption',
-//     img: corruption,
-//     text: `
-//       <p>It is our policy to conduct our business activities with honesty, integrity and to the highest ethical standards.</p>
-
-//       Read the MTN-GROUP ANTI-BRIBERY & CORRUPTION POLICY here </br>
-//       <a href="https://bit.ly/30Tr73B">https://bit.ly/30Tr73B</a>`
-//   },
-//   {
-//     heading: 'Conflict of Interest',
-//     img: interest,
-//     text: `
-//       <p>It is not permissible for employees to: </p>
-
-//       <ul>
-//       <li> conclude contracts or arrangements or receive or place business on behalf of MTN for financial or personal gain;</li>
-//       <li>use his/her position or authority to influence or make decisions that lead to any form of financial or personal gain for him/herself, his/her close associates or family members; </li>
-//       <li>source or promote any commercial activities for a third party for the purpose of financial or personal gain for him/herself,
-//       his/her close associates or family members; and/or engage in any other conduct that may be construed or identified as constituting a conflict of interest.</li>
-//       </ul>
-
-//       Read the Conflict of Interest Policy here <a href="https://bit.ly/30Tr73B">https://bit.ly/30Tr73B</a>`
-//   },
-//   {
-//     heading: 'Gifts, Entertainment and Hospitality',
-//     img: gifts,
-//     text: `
-//       <p>Whilst we appreciate our business partners’ goodwill, MTN has a strict “No-Gifts” stance.</p>
-
-//       Read the MTN-Goup Gifts & Entertainment Policy here <a href="https://bit.ly/30Tr73B">https://bit.ly/30Tr73B</a>`
-//   },
-//   {
-//     heading: 'Our Employees',
-//     img: employees,
-//     text: `
-//       <p>“We believe in developing our talent, because it’s our people who make us what we are.”</p>
-
-//       <p>At MTN, we do not tolerate harassment, intimidation or discrimination and we respect the political and religious views of all employees. We are also strongly against any form of slavery and child labor.</p>
-
-//       <p>It is important that you use the link below to read MTN Disciplinary Code and Policy. You may also refer to your local Risk and Compliance Department for more information. <a href="https://bit.ly/2SCe2ca">https://bit.ly/2SCe2ca</a></a></p>`
-//   },
-//   {
-//     heading: 'Our Customers',
-//     img: customers,
-//     text: `
-//     <p>Our relationship with our customers is based trust and integrity and we are committed to upholding these values throughout the
-//     organization and in how we serve our customers. Through our collective commitment, we are able to work together to ensure that we make our customers’ lives a whole lot BRIGHTER.</p>`
-//   },
-//   {
-//     heading: 'Data Privacy',
-//     img: privacy,
-//     text: `
-//       <p>An information leak takes place when confidential information is revealed to unauthorized persons or parties.
-//       Employees who are tempted on a regular basis to disclose customer information to family members or friends, contrary to the data protection laws which MTN is required to operate within should desist from the act.</p>
-
-//       Read the MTN-Goup Gifts & Entertainment Policy here <a href="https://bit.ly/30Tr73B">https://bit.ly/30Tr73B</a>
-//   `
-//   },
-//   {
-//     heading: 'Public Relations',
-//     img: pr,
-//     text: `<p>
-//       What is required of us?
-//       We ensure that any statements disclosed to the public and/or key stakeholders are accurate, factual and are disclosed by authorized officials.
-//       </p>
-//       `
-//   },
-//   {
-//     heading: 'Our Suppliers',
-//     img: suppliers,
-//     text: `
-//       <p>We ensure that our vendors and suppliers adhere to our ethical standards and compliance obligations at all times and wherever we operate in the world.
-//       The MTN Group Supplier Code of Conduct defines the standards to be met by all suppliers in carrying out business with MTN, including their employees and subcontractors.</p>`
-//   },
-//   {
-//     heading: 'Compliance and Governance',
-//     img: governance,
-//     text: `
-//      <p> We encourage our stakeholders to take personal responsibility for upholding the Group’s ethics, but we also have formal structures in place to assist with compliance.</p>`
-//   },
-//   {
-//     heading: 'Communities, Society and the Environment',
-//     img: society,
-//     text: `
-//       <p>In order to ensure a sustainable future for all, we must ensure that we keep up to date with environmental laws, regulations and international best practice. We must also use our resources sparingly and in a prudent manner at all times. </p>
-
-//       <p>Use water, electricity and other resources wisely. Think before printing, dispose of waste responsibly and recycle waste in designated areas.</p>`
-//   },
-//   {
-//     heading: 'Guidance',
-//     img: guidance,
-//     text: `
-//       <p>We see the choice to communicate and speak up as a proactive step in managing any issues or raising concerns.</p>
-
-//       <p>If you require guidance on our ethical standards or you suspect or have witnessed any breach of the standards set out in the MTN Conduct Passport, please consider consult with your direct line management.</p>`
-//   }
-// ]
-
 class Compliance extends Component {
   state = {
     checked: false
@@ -243,6 +113,9 @@ class Compliance extends Component {
     const sections = pageContent
       ? pageContent.filter(section => section.order > 0)
       : []
+    const sortedSections = sections
+      ? sections.sort((a, b) => (a.order > b.order ? 1 : -1))
+      : []
 
     return (
       <div>
@@ -261,8 +134,8 @@ class Compliance extends Component {
           </Hero>
 
           <Wrapper width="50%">
-            {sections &&
-              sections
+            {sortedSections &&
+              sortedSections
                 // .filter(section => section.order > 0)
                 .map((section, id) => (
                   <Section key={id}>
